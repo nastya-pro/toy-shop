faef846aec7cb410762e47e951cdc8d8c8013308
Nastya <anast.ars@yandex.ru>
2019-11-07 16:57:59 +0400

    Добавлена html страница

--- src/main/kotlin/ru/rsreu/toy/shop/Controller.kt
+++ src/main/kotlin/ru/rsreu/toy/shop/Controller.kt
@@ -10,10 +10,8 @@ class Controller {
     @GetMapping(value=["/products"], produces = [MediaType.APPLICATION_JSON_VALUE])
     fun products(): List<Product> {
         val product1=Product("Мишка", "img/Мишка.jpg","Милый плюшевый мишка станет лучшим другом вашему ребенку.","023498", 1700)
-        return listOf(product1)
         val product2=Product("Пикачу","img/Пикачу.jpg","Детектив Пикачу поможет найти сладости и конечно же кофе.","190384",987)
-        return listOf(product2)
         val product3=Product("Пингвинёнок","img/Пингвинёнок.jpg","Мягкий пингвинёнок скрасит холодные и грустные вечера.","193278",1500)
-        return listOf(product3)
+        return listOf(product1,product2,product3)
     }
 }
