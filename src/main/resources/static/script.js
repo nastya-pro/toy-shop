7554eb9e0c574660d51e370191f948e663e14baa
Nastya <anast.ars@yandex.ru>
2019-11-07 15:41:15 +0400

    Добавлена html страница

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,54 @@
+let testProducts = [
+    {
+        title: "Мишка",
+        imgUrl: "img/Мишка.jpg",
+        description: "Милый плюшевый мишка станет лучшим другом вашему ребенку.",
+        vendorCode: "023498",
+        price: 1700
+    },
+    {
+        title: "Пикачу",
+        imgUrl: "img/Пикачу.jpg",
+        description: "Детектив Пикачу поможет найти сладости и конечно же кофе.",
+        vendorCode: "190384",
+        price: 987
+    },
+    {
+        title: "Пингвинёнок",
+        imgUrl: "img/Пингвинёнок.jpg",
+        description: "Мягкий пингвинёнок скрасит холодные и грустные вечера.",
+        vendorCode: "193278",
+        price: 1500
+    }
+];
+
+function testFill(){
+    fillProducts(testProducts)
+}
+
+// window.onload = getProducts;
+window.onload=testFill;
+
+function fillProducts(products) {
+    let box = $("#products");
+    let productTemplateHtml = $("#product").html();
+    let productTemplate = _.template(productTemplateHtml);
+
+    for(let i=0; i<products.length; i++) {
+        let filledTemplate = productTemplate(products[i]);
+        box.append(filledTemplate);
+    }
+}
+
+function onError() {
+    console.log("Fail")
+}
+
+function getProducts() {
+    $.ajax({
+        type:'get',
+        url: '/products',
+        contentType: 'application/json'
+    }).done(fillProducts)
+        .fail(onError)
+}
