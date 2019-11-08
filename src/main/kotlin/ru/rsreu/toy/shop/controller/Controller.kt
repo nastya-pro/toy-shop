35e00247c974e0d00561455b365778d92073b8e0
Nastya <anast.ars@yandex.ru>
2019-11-08 23:18:04 +0400

    Репозиторий подключен к контроллеру

--- src/main/kotlin/ru/rsreu/toy/shop/controller/Controller.kt
+++ src/main/kotlin/ru/rsreu/toy/shop/controller/Controller.kt
@@ -4,15 +4,27 @@ import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import ru.rsreu.toy.shop.dto.ProductDto
+import ru.rsreu.toy.shop.entity.Product
+import ru.rsreu.toy.shop.repository.ProductRepository
+import javax.swing.text.html.parser.Entity
 
 @RestController
-class Controller {
+class Controller(
+    private val productRepository: ProductRepository
+) {
 
     @GetMapping(value = ["/products"], produces = [MediaType.APPLICATION_JSON_VALUE])
     fun products(): List<ProductDto> {
-        val product1 = ProductDto("Мишка", "img/Мишка.jpg", "Милый плюшевый мишка станет лучшим другом вашему ребенку.", "023498", 1700)
-        val product2 = ProductDto("Пикачу", "img/Пикачу.jpg", "Детектив Пикачу поможет найти сладости и конечно же кофе.", "190384", 987)
-        val product3 = ProductDto("Пингвинёнок", "img/Пингвинёнок.jpg", "Мягкий пингвинёнок скрасит холодные и грустные вечера.", "193278", 1500)
-        return listOf(product1, product2, product3)
+        return productRepository.findAll().map { createResult(it) }
+    }
+
+    private fun createResult(entity: Product): ProductDto {
+        return ProductDto(
+            title = entity.title,
+            description = entity.description,
+            imgUrl = entity.imgUrl,
+            price = entity.price,
+            vendorCode = entity.vendorCode
+        )
     }
 }
--- src/main/kotlin/ru/rsreu/toy/shop/entity/Product.kt
+++ src/main/kotlin/ru/rsreu/toy/shop/entity/Product.kt
@@ -2,11 +2,13 @@ package ru.rsreu.toy.shop.entity
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import org.springframework.data.mongodb.core.mapping.Field
 import java.util.*
 
 @Document("PRODUCTS")
 data class Product(
     @Id
+    @Field("_id")
     val id: UUID,
     val title: String,
     val imgUrl: String,
